742bdd1a4daa75a36294e2afb5e6ea725d49face
Thiago <talarini@gmail.com>
2018-01-07 12:22:20 -0200

    Edita receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,14 +16,28 @@ class RecipesController < ApplicationController
     else
       render '_error'
     end
+  end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render '_error'
+    end
   end
 
 
-  private
 
-  def recipe_params
-    params.require(:recipe).permit(:title, :recipe_type_id, :cuisine_id, :difficulty, :cook_time, :ingredients, :method, :portion)
-  end
+private
+
+def recipe_params
+  params.require(:recipe).permit(:title, :recipe_type_id, :cuisine_id, :difficulty, :cook_time, :ingredients, :method, :portion)
+end
 
 end
